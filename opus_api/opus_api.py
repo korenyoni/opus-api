6869e2cc2a4d594eb92e94997aa18c74626b64b3
Yonatan Koren <yonkornilov@live.com>
2017-10-03 15:18:05 -0400

    Cli has first command

--- opus_api/opus_api.py
+++ opus_api/opus_api.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+import bs4
+import site_cache
+
+"""
+Main module.
+
+Provides OPUS queries
+"""
+
+sc = site_cache.SiteCache()
+c = sc.get_site()
+soup = bs4.BeautifulSoup(c, 'html.parser')
+
+
+def get_title():
+    return soup.title
