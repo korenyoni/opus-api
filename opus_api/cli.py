e1953f696412248ab4ac0191c72a9d4c3e52084f
Yonatan Koren <yonkornilov@live.com>
2017-10-03 09:37:27 -0400

    ignoring .venv, changed makefile to delete .egg recursively, added ascii art in help

--- opus_api/cli.py
+++ opus_api/cli.py
@@ -7,11 +7,12 @@ import click
 
 @click.command()
 def main(args=None):
-    """Console script for opus_api."""
-    click.echo("Replace this message by putting your code into "
-               "opus_api.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-
+    """
+     ,---.     ,---..   .,---.
+,---.|   |,---.|---'|   |`---.
+|    |   ||    |    |   |    |
+`---'`---'`    `    `---'`---'
+    """
 
 if __name__ == "__main__":
     main()
