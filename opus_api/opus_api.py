682d5cf4d6a3c5e0d316c7408f3ba1984f1f3018
Yonatan Koren <yonkornilov@live.com>
2017-10-04 15:20:53 -0400

    added proper docstrings in opus_api

--- opus_api/opus_api.py
+++ opus_api/opus_api.py
@@ -17,14 +17,26 @@ Provides OPUS queries
 
 
 def jsonify(to_jsonify):
+    """
+    Internal function to dump dicts into json format.
+    """
     return json.dumps(to_jsonify, indent=2, sort_keys=True)
 
 
 def get_size(url):
+    """
+    Internal function to get size of url.
+    OPUS url heads don't have content-length, so the url must be opened
+    in order to calculate the size.
+    """
     return urllib.urlopen(url).info()['content-length']
 
 
 def checkLangs(src, target):
+    """
+    Ensure src and target are both valid,
+    raise specific exceptions in case either are not.
+    """
     valid_langs = json.loads(langs())['langs']
     if src not in valid_langs:
         raise InvalidSrcException(src)
@@ -34,6 +46,9 @@ def checkLangs(src, target):
 
 @jcache
 def get(src, target):
+    """
+    Get corpora for src-target
+    """
     checkLangs(src, target)
 
     html = crawler.get(src, target)
@@ -57,6 +72,9 @@ def get(src, target):
 
 @jcache
 def langs():
+    """
+    Get list of both src and target languages
+    """
     html = requests.get(settings.site_url).content
     crawl_soup = bs4.BeautifulSoup(html, 'html.parser')
 
