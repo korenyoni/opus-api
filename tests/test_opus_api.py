12768a01d0282309fd7ba73d6fbabb7109515595
Yonatan Koren <yonkornilov@live.com>
2017-10-05 20:44:58 -0400

    added first test case

--- tests/test_opus_api.py
+++ tests/test_opus_api.py
@@ -20,15 +20,18 @@ class TestOpus_api(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_000_langs(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.main, ['langs'])
+        assert result.exit_code == 0
+        assert "zsm (zsm)" in result.output
 
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'opus_api.cli.main' in result.output
+        assert 'OPUS (opus.lingfil.uu.se)' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert 'Show this message and exit.' in help_result.output
