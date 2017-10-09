62ea8736572f8a9f7ebdc1784532fa3e4c8e0396
Yonatan Koren <yonkornilov@live.com>
2017-10-09 13:12:29 -0400

    adding suspected C dependent libraries to mock

--- docs/conf.py
+++ docs/conf.py
@@ -281,5 +281,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['opus_api']
+MOCK_MODULES = ['click','diskcache']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
