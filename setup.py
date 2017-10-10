d3656b1193992446fd68210f1ac473a33f512957
Yonatan Koren <yonkornilov@live.com>
2017-10-10 12:45:18 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='opus_api',
-    version='0.2.6',
+    version='0.2.7',
     description="OPUS (opus.lingfil.uu.se) Python API",
     long_description="OPUS (opus.lingfil.uu.se) Python2 API and CLI.\
     Read the `documentation <http://opus-api.readthedocs.io>`__.",
